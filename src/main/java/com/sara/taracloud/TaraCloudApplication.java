abdfb50a1f3f88b97ea062dcee51203a6928624e
saraHH1417 <sara.afzar@gmail.com>
2022-12-23 20:52:39 +0330

    feat: chapter one creating HomeController

---
+++ src/main/java/com/sara/taracloud/TaraCloudApplication.java
@@ -0,0 +1,13 @@
+package com.sara.taracloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaraCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaraCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sara/taracloud/TaraCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.taracloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaraCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
